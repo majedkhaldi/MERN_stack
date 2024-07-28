bc1323799adf901e0f61a5862574fcef7da0b159
Majed Alkhaldi <majed_khaldi@hotmail.com>
2024-07-28 13:09:26 +0300

    day3 assignment 2

---
+++ assignemnts/react/tryjsx/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to know</h2>
+      <li>Learn REact</li>
+      <li>Climb mt. Everest</li>
+      <li>Run a Marathon</li>
+      <li>Feed the dogs</li>
+    </div>
+  );
+}
+
+export default App;

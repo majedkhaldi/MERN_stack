0f08160264b8d37921c78be4c66eebda01fad509
Majed Alkhaldi <majed_khaldi@hotmail.com>
2024-07-30 14:54:37 +0300

    day5 assignment 2

--- assignemnts/react/hookform/src/UserForm.js
+++ assignemnts/react/hookform/src/UserForm.js
@@ -6,8 +6,66 @@ const UserForm = () => {
     const [Lastname, setLastname] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");  
-    const [cpassword, setcPassword] = useState("");  
-    
+    const [cpassword, setcPassword] = useState("");
+    const [fnameError, setFNameError] = useState("");
+    const [lnameError, setLNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setpasswordError] = useState("");
+    const [matchError, setmatchError] = useState("");
+  
+    const handleFName = (e) => {
+        setFirstname(e.target.value)
+        if(e.target.value.length < 1) {
+            setFNameError("Title is required!");
+        } else if(e.target.value.length < 2) {
+            setFNameError("First Name must be 2 characters or longer!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setFNameError("");
+        }
+    }
+    const handleLName = (e) => {
+        setLastname(e.target.value)
+        if(e.target.value.length < 1) {
+            setLNameError("Title is required!");
+        } else if(e.target.value.length < 2) {
+            setLNameError("Last Name must be 2 characters or longer!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setLNameError("");
+        }
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+        if(e.target.value.length < 1) {
+            setEmailError("Title is required!");
+        } else if(e.target.value.length < 5) {
+            setEmailError("Email must be 5 characters or longer!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setEmailError("");
+        }
+    }
+    const handlePass = (e) => {
+        setPassword(e.target.value)
+        if(e.target.value.length < 1) {
+            setpasswordError("Password is required!");
+        } else if(e.target.value.length < 8) {
+            setpasswordError("Password must be 8 characters or longer!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setpasswordError("");
+        }
+    }
+    const handlePassmatch = (e,password) => {
+        setcPassword(e.target.value)
+        if(e.target.value != password) {
+            setmatchError("Passwords don't match");
+        } else {
+            setmatchError("");
+        }
+    }
+
     const createUser = (e) => {
         // we must prevent the default refresh of the browser to keep our state from being reset
         e.preventDefault();
@@ -27,32 +85,53 @@ const UserForm = () => {
         <form>
             <div className={styleee.field}>
                 <label>Firstname: </label> 
-                <input type="text" value={Firstname} onChange={ (e) => setFirstname(e.target.value) } />
+                <input type="text" value={Firstname} onChange={ handleFName } />
             </div>
+            <div>{fnameError}</div>
             <div className={styleee.field}>
                 <label>Lastname: </label> 
-                <input type="text" value={Lastname} onChange={ (e) => setLastname(e.target.value) } />
+                <input type="text" value={Lastname} onChange={ handleLName } />
             </div>
+            <div className={styleee.errorm}>{
+                    lnameError ?
+                    <p>{ lnameError }</p> :
+                    ''
+                }</div>
             <div className={styleee.field}>
                 <label>Email Address: </label> 
-                <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } />
+                <input type="text" value={email} onChange={ handleEmail } />
             </div>
+            <div>{
+                    emailError ?
+                    <p className={styleee.errorm}>{ emailError }</p> :
+                    ''
+                }</div>
             <div className={styleee.field}>
                 <label>Password: </label>
-                <input type="password" value={password} onChange={ (e) => setPassword(e.target.value) } />
+                <input type="password" value={password} onChange={ handlePass } />
             
             </div>
+            <div className={styleee.errorm}>{
+                    passwordError ?
+                    <p>{ passwordError }</p> :
+                    ''
+                }</div>
             <div className={styleee.field}>
                 <label>confirm Password: </label>
-                <input type="password" value={cpassword} onChange={ (e) => setcPassword(e.target.value) } />
+                <input type="password" value={cpassword} onChange={ handlePassmatch } />
             
             </div>
+            <div className={styleee.errorm}>{
+                    matchError ?
+                    <p>{ matchError }</p> :
+                    ''
+                }</div>
         </form>
         <p>first name: <h5 className={styleee.answer}>{Firstname}</h5></p>
-        <p>last name: <h5 className='answer'>{Lastname}</h5></p>
-        <p>email: <h5 className='answer'>{email}</h5></p>
-        <p>password: <h5 className='answer'>{password}</h5></p>
-        <p>confrim password: <h5 className='answer'>{cpassword}</h5></p>
+        <p>last name: <h5 className={styleee.answer}>{Lastname}</h5></p>
+        <p>email: <h5 className={styleee.answer}>{email}</h5></p>
+        <p>password: <h5 className={styleee.answer}>{password}</h5></p>
+        <p>confrim password: <h5 className={styleee.answer}>{cpassword}</h5></p>
         
         </>
     );

f5a78426f126c5d4a6ab9f70841ef965dcf09b44
Majed Alkhaldi <majed_khaldi@hotmail.com>
2024-07-31 12:40:04 +0300

    day6 assignment 1

---
+++ assignemnts/react/pokemon/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Pokemon from './Pokemon'
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+function App() {
+
+  return (
+    <>
+    <Pokemon/>
+    </>
+  )
+}
+
+export default App
